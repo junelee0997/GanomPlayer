662392abae5feb7a22a0f9fc7c954869eba45328
junelee0997 <hclee0945@gmail.com>
2023-12-26 11:17:02 +0900

    test

---
+++ activate.py
@@ -0,0 +1,2 @@
+def run(data:dict):
+    return data
---
+++ server.py
@@ -0,0 +1,42 @@
+import socket
+import activate
+import json
+# socket details
+IP = '127.0.0.1'
+PORT = 25566
+SIZE = 1024
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((IP, PORT))
+server_socket.listen()
+print(f"Server start in {IP}")
+
+try:
+    while True:
+                ch = 1
+                print("Ready to accept")
+                client_socket, client_addr = server_socket.accept()
+                while True:
+                    msg = client_socket.recv(SIZE)
+                    if not msg:
+                        print("Ended the connection")
+                        ch = 0
+                        break
+                    if msg == b'-1':
+                        client_socket.send("Train pended".encode())
+                        print("Client pended the connection")
+                        break
+                    msg = eval(msg.decode("utf-8"))
+                    print(msg)
+                    recv = json.dumps(activate.run(msg))
+                    client_socket.send(recv.encode("utf-8"))
+                if not ch:
+                    break
+
+except Exception as e:
+        print('에러 : ', e)
+
+finally:
+        print("done")
+        server_socket.close()
