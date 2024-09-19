0773567f8a73684b3d64f577e1d5eef23ef97e8d
junelee0997 <hclee0945@gmail.com>
2024-09-19 21:45:50 +0900

    Graph

--- graph.py
+++ graph.py
@@ -6,7 +6,7 @@ data2 = pd.read_csv(loss_path + '/dloss.csv')
 whole_g_loss = list(data['Generator Loss'])
 whole_d_loss = list(data2['Discriminator Loss'])
 index = [i + 1 for i in range(len(whole_g_loss))]
-index2 = [i + 1 for i in range(len(whole_d_loss))]
+index2 = [(i + 1) * len(whole_g_loss) // len(whole_d_loss) for i in range(len(whole_d_loss))]
 plt.plot(index[:], whole_g_loss[:], label='Generator Loss')
 plt.plot(index2[:], whole_d_loss[:], label='Discriminator Loss')
 plt.legend()
